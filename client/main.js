ebaca5e060057536165c2d2257176d8aabffc0e3
Chicloon <Chicloon@gmail.com>
2017-06-06 22:36:44 +0900

    lesson 3.15

--- client/main.js
+++ client/main.js
@@ -1,1 +1,36 @@
-console.log('Log from ./client/main.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+
+const players = [{
+    _id: 1,
+    name: 'Mike',
+    score: 3,
+}, {
+    _id: 2,
+    name: 'Lora',
+    score: 12
+}, {
+    _id: 3,
+    name: 'Jhon',
+    score: 123
+}];
+
+const renderPlayers = () => {
+    return players.map(e => <p key={e._id}> {e.name} has {e.score} point(s).</p>);
+
+}
+
+        // {players.map(e=> <p key={e._id}> {e.name} </p>)}
+Meteor.startup(() => {
+    const title = 'Score Keep'
+    const name = 'Mike';
+    const jsx = (
+        <div>
+            <h1> {title} </h1>
+            <p> Hello, {name} from main.js</p>
+{renderPlayers()}
+        </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ import '../imports/utils.js';
 import someDefault, { greetUser } from '../imports/utils.js';
 import add  from '../imports/math';
 
-console.log(add(23,2));
+console.log(add(2,2));
 console.log('Log from ./server/main.js');
 
 console.log(greetUser(), someDefault);
